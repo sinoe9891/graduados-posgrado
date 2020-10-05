b534f36fda2cee26938d4e2287a02b116b42c45c
sinoe9891 <sinoeproducciones@gmail.com>
2020-10-05 16:47:27 -0600

    header, footer

--- index.php
+++ index.php
@@ -5,114 +5,20 @@
 <!-- Bootstrap CSS -->
 
 <link rel="stylesheet" type="text/css" href="css/style-profile.css">
-    <link rel="stylesheet" type="text/css" href="includes/styles/back-to-top/back-to-top.css">
+<link rel="stylesheet" type="text/css" href="includes/styles/back-to-top/back-to-top.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <style>
-	.container1 {
-		width: 100% !important;
-		display: flex;
-		justify-content: center;
-	}
 
-	.container_main {
-		width: 720px;
-		margin: 50px 0;
-		display: flex;
-		height: 392px;
-		box-shadow: 3px 3px #0000005c;
-	}
-
-	.titulo {
-		text-align: right;
-		padding: 20px 20px;
-	}
-
-	.titulo h3 {
-		color: #931919;
-		line-height: 1;
-		font-size: 28px;
-	}
-
-	.titulo h1 {
-		color: #931919;
-		line-height: 1;
-		font-size: 28px;
-	}
-
-	.column_one {
-		width: 350px;
-	}
-
-	.column_one img {
-		width: 100%;
-		object-fit: cover;
-		height: 392px;
-	}
-
-	.column_second {
-		width: 350px;
-	}
-
-
-	form {
-		display: flex;
-		justify-content: center;
-	}
-
-	label {
-		padding: 0 10px;
-		text-align: right;
-		display: inline-block;
-		width: 80px;
-	}
-
-	#claseInput,
-	#codigoInput,
-	#nombresInput,
-	#apellidosInput,
-	#paisInput {
-		border-radius: 50px !important;
-		width: 150px;
-		height: 25px;
-		background: #cdcdcd;
-	}
-
-	#paisInput {
-		border: none;
-	}
-
-
-	#buscar {
-		background: #008341;
-		color: white;
-		width: 100px;
-		margin-top: 20px;
-	}
-
-	input {
-		width: 147px;
-		border-radius: 50px;
-	}
-
-	select {
-		text-align-last: right;
-		text-align: right;
-		-ms-text-align-last: right;
-		-moz-text-align-last: right;
-	}
-
-	.input {
-		display: flex;
-		margin-bottom: 5px;
-		width: 230px;
-		display: flex;
-		justify-content: center;
-
-	}
 </style>
 
 <body>
+	<nav class="navbar navbar-expand-lg">
+		<a class="navbar-brand" href="#">
+			<img src="https://www.zamorano.edu/wp-content/uploads/2018/10/zamorano_logo_blanco-02.png" alt="">
+		</a>
+	</nav>
+
 	<main class="container">
 		<div>
 			<div class="row">
